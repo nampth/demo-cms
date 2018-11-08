60f103208270b14968d47acfd1e0f5f2c83be731
nampth1 <annamphu95@gmail.com>
2018-11-08 17:17:19 +0700

    update db

--- database.sql
+++ database.sql
@@ -51,7 +51,7 @@ CREATE TABLE `permission_role` (
   `role_id` varchar(45) DEFAULT NULL,
   `permission_id` varchar(45) DEFAULT NULL,
   PRIMARY KEY (`id`)
-) ENGINE=InnoDB AUTO_INCREMENT=3 DEFAULT CHARSET=latin1;
+) ENGINE=InnoDB AUTO_INCREMENT=48 DEFAULT CHARSET=latin1;
 /*!40101 SET character_set_client = @saved_cs_client */;
 
 --
@@ -60,7 +60,7 @@ CREATE TABLE `permission_role` (
 
 LOCK TABLES `permission_role` WRITE;
 /*!40000 ALTER TABLE `permission_role` DISABLE KEYS */;
-INSERT INTO `permission_role` VALUES (1,'1','1'),(2,'1','2');
+INSERT INTO `permission_role` VALUES (41,'1','1'),(42,'1','2'),(43,'1','3'),(44,'1','4'),(47,'9','5');
 /*!40000 ALTER TABLE `permission_role` ENABLE KEYS */;
 UNLOCK TABLES;
 
@@ -76,7 +76,7 @@ CREATE TABLE `permissions` (
   `name` varchar(100) CHARACTER SET latin1 DEFAULT NULL,
   `description` varchar(255) DEFAULT NULL,
   PRIMARY KEY (`id`)
-) ENGINE=InnoDB AUTO_INCREMENT=3 DEFAULT CHARSET=utf8mb4;
+) ENGINE=InnoDB AUTO_INCREMENT=6 DEFAULT CHARSET=utf8mb4;
 /*!40101 SET character_set_client = @saved_cs_client */;
 
 --
@@ -85,7 +85,7 @@ CREATE TABLE `permissions` (
 
 LOCK TABLES `permissions` WRITE;
 /*!40000 ALTER TABLE `permissions` DISABLE KEYS */;
-INSERT INTO `permissions` VALUES (1,'admin_dashboard','Truy cập màn hình dashboard của Admin'),(2,'user_management','Quản trị người dùng');
+INSERT INTO `permissions` VALUES (1,'admin_dashboard','Truy cập màn hình dashboard của Admin'),(2,'user_management','Quản trị người dùng'),(3,'role_management','Quản trị nhóm người dùng'),(4,'permission_management','Quản trị quyền người dùng'),(5,'user_dashboard','Truy cập dashboard của user thường');
 /*!40000 ALTER TABLE `permissions` ENABLE KEYS */;
 UNLOCK TABLES;
 
@@ -102,7 +102,7 @@ CREATE TABLE `roles` (
   `description` varchar(256) DEFAULT NULL,
   `default_redirect` varchar(256) CHARACTER SET latin1 DEFAULT NULL,
   PRIMARY KEY (`id`)
-) ENGINE=InnoDB AUTO_INCREMENT=2 DEFAULT CHARSET=utf8mb4;
+) ENGINE=InnoDB AUTO_INCREMENT=10 DEFAULT CHARSET=utf8mb4;
 /*!40101 SET character_set_client = @saved_cs_client */;
 
 --
@@ -111,7 +111,7 @@ CREATE TABLE `roles` (
 
 LOCK TABLES `roles` WRITE;
 /*!40000 ALTER TABLE `roles` DISABLE KEYS */;
-INSERT INTO `roles` VALUES (1,'admin','Administrator','/dashboard');
+INSERT INTO `roles` VALUES (1,'admin','Administrator','/admin/dashboard'),(9,'normal','Người dùng thông thường','/');
 /*!40000 ALTER TABLE `roles` ENABLE KEYS */;
 UNLOCK TABLES;
 
@@ -127,7 +127,7 @@ CREATE TABLE `user_role` (
   `user_id` varchar(45) DEFAULT NULL,
   `role_id` varchar(45) DEFAULT NULL,
   PRIMARY KEY (`id`)
-) ENGINE=InnoDB AUTO_INCREMENT=2 DEFAULT CHARSET=latin1;
+) ENGINE=InnoDB AUTO_INCREMENT=72 DEFAULT CHARSET=latin1;
 /*!40101 SET character_set_client = @saved_cs_client */;
 
 --
@@ -136,7 +136,7 @@ CREATE TABLE `user_role` (
 
 LOCK TABLES `user_role` WRITE;
 /*!40000 ALTER TABLE `user_role` DISABLE KEYS */;
-INSERT INTO `user_role` VALUES (1,'1','1');
+INSERT INTO `user_role` VALUES (1,'1','1'),(71,'3','9');
 /*!40000 ALTER TABLE `user_role` ENABLE KEYS */;
 UNLOCK TABLES;
 
@@ -150,13 +150,15 @@ DROP TABLE IF EXISTS `users`;
 CREATE TABLE `users` (
   `id` int(11) NOT NULL AUTO_INCREMENT,
   `username` varchar(45) CHARACTER SET latin1 NOT NULL,
+  `fullname` varchar(100) DEFAULT NULL,
+  `email` varchar(100) DEFAULT NULL,
   `password` varchar(100) CHARACTER SET latin1 NOT NULL,
   `status` varchar(45) CHARACTER SET latin1 DEFAULT NULL,
   `remember_token` varchar(100) CHARACTER SET latin1 DEFAULT NULL,
   `created_at` varchar(45) CHARACTER SET latin1 DEFAULT NULL,
   `updated_at` varchar(45) CHARACTER SET latin1 DEFAULT NULL,
   PRIMARY KEY (`id`)
-) ENGINE=InnoDB AUTO_INCREMENT=2 DEFAULT CHARSET=utf8mb4;
+) ENGINE=InnoDB AUTO_INCREMENT=4 DEFAULT CHARSET=utf8mb4;
 /*!40101 SET character_set_client = @saved_cs_client */;
 
 --
@@ -165,7 +167,7 @@ CREATE TABLE `users` (
 
 LOCK TABLES `users` WRITE;
 /*!40000 ALTER TABLE `users` DISABLE KEYS */;
-INSERT INTO `users` VALUES (1,'nampth','$2y$10$tY.rJ4g/zaw0Rn.l0q4nCO/mnDTDJT.vUFZ.YLtHQIYVFud1qanJO','0','iF2X5Nxgpvc9gbHkUIu89jjzzajC62KEMRD36vdMfX4wfPzuZ1JPlnvt0Rtz',NULL,NULL);
+INSERT INTO `users` VALUES (1,'nampth','Phan Nam','annamphu95@gmail.com','$2y$10$tY.rJ4g/zaw0Rn.l0q4nCO/mnDTDJT.vUFZ.YLtHQIYVFud1qanJO','0','IKAjXMId4YfdNwccTPWytAOiT0JLQk3BWVLdTarLTWEDmhT2ty99LlAhCkdf',NULL,'2018-11-08 08:15:49'),(3,'test123','Phan Nam hihi','nampth@kalapa.vn','$2y$10$VHxMi8eyePfM1PGzFvlIqufkT/tcQlVBeY9iTVrPgTNF7a4YDx.oi','0',NULL,'2018-11-08 09:09:09','2018-11-08 09:47:28');
 /*!40000 ALTER TABLE `users` ENABLE KEYS */;
 UNLOCK TABLES;
 /*!40103 SET TIME_ZONE=@OLD_TIME_ZONE */;
@@ -178,4 +180,4 @@ UNLOCK TABLES;
 /*!40101 SET COLLATION_CONNECTION=@OLD_COLLATION_CONNECTION */;
 /*!40111 SET SQL_NOTES=@OLD_SQL_NOTES */;
 
--- Dump completed on 2018-10-24 11:03:20
+-- Dump completed on 2018-11-08 17:16:32
